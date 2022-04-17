7a9592aada2d4fc9067fdea44c231f77a8de2fa9
Carl Harrison <carlharrison1@live.co.uk>
2022-04-17 09:01:44 +0100

    Filled containers and added add letter funcitonality to one row. Mapping across rows partially done

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { borderLeftColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { borderColor, borderLeftColor, color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 import { colors } from './src/constants';
 import Keyboard from './src/components/Keyboard';
 
+const NUMBER_OF_TRIES = 6;
+
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+}
+
 export default function App() {
+
+  const word = "hello";
+  const letters = word.split('');
+
+  const [rows, setRows] = useState(new Array(NUMBER_OF_TRIES).fill(
+    new Array(letters.length).fill('')
+  ));
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const onKeyPressed = (key) => {
+    const updateRows = copyArray(rows);
+    updateRows[curRow][curCol] = key;
+    setRows(updateRows);
+    setCurCol(curCol + 1);
+  }
+
+  const isCellActive = (row, col) => {
+    return row == curRow && col == curCol;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <Text style={styles.title}>WORDLE</Text>
-
-      <View style={styles.map}>
-        <View style={styles.row}>
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-        </View>
-      </View>
-
-      <Keyboard />
+      <ScrollView style={styles.map}>
+        {rows.map((row, i) => (
+          <View style={styles.row}>
+            {row.map((cell, j) => (
+              <View style={[
+                styles.cell,
+                {
+                  borderColor: isCellActive(i, j)
+                    ? colors.lightgrey
+                    : colors.darkgrey
+                }
+              ]}>
+                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+      <Keyboard onKeyPressed={onKeyPressed} />
     </SafeAreaView>
   );
 }
@@ -36,5 +71,30 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
     letterSpacing: 7
+  },
+  map: {
+    alignSelf: "stretch",
+    height: 100,
+    marginVertical: 20,
+  },
+  row: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  cell: {
+    borderWidth: 3,
+    borderColor: colors.darkgrey,
+    flex: 1,
+    aspectRatio: 1,
+    margin: 3,
+    maxWidth: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellText: {
+    color: colors.lightgrey,
+    fontWeight: "bold",
+    fontSize: 28,
   }
 });
